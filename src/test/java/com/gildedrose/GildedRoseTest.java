b1d80e327638708a1495bb9bf7d852eee8485eb8
chirich <cdchiriac@gmail.com>
2022-09-01 22:26:59 +0200

    removed unused import

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,7 +3,6 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
